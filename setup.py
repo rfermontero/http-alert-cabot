14d8719d9b0abef78b366921b9f572c96887b40e
rfernandez <rfernandez@tuenti.com>
2015-07-02 15:07:11 +0200

    first commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-setup(name='cabot-http-alert',
+setup(name='cabot-http-alert-plugin',
       version='0.1',
       description='A Cabot plugin for sending alerts over http post',
       author='Roberto Fernandez',
