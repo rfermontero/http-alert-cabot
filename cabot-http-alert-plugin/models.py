8e6fa74bc0aff33d3cab686cddeda8a20d6f5930
rfernandez <rfernandez@tuenti.com>
2015-07-02 12:57:30 +0200

    first commit

---
+++ cabot-http-alert-plugin/models.py
@@ -0,0 +1,61 @@
+from cabot.cabotapp.alert import AlertPlugin, AlertPluginUserData
+
+from django.db import models
+from django.conf import settings
+from django.template import Context, Template
+from os import environ as env
+
+import requests
+
+post_template = "Service {{ service.name }} {% if service.overall_status == service.PASSING_STATUS %}is back to normal{% else %}reporting {{ service.overall_status }} status{% endif %}: {{ scheme }}://{{ host }}{% url 'service' pk=service.id %}. {% if service.overall_status != service.PASSING_STATUS %}Checks failing: {% for check in service.all_failing_checks %}{% if check.check_category == 'Jenkins check' %}{% if check.last_result.error %} {{ check.name }} ({{ check.last_result.error|safe }}) {{jenkins_api}}job/{{ check.name }}/{{ check.last_result.job_number }}/console{% else %} {{ check.name }} {{jenkins_api}}/job/{{ check.name }}/{{check.last_result.job_number}}/console {% endif %}{% else %} {{ check.name }} {% if check.last_result.error %} ({{ check.last_result.error|safe }}){% endif %}{% endif %}{% endfor %}{% endif %}{% if alert %}{% for alias in users %} @{{ alias }}{% endfor %}{% endif %}"
+
+
+class HttpAlert(AlertPlugin)
+
+	name = "AlertHttp"
+	author = "rfernandez"
+
+    def send_alert(self, service, users, duty_officers):
+
+    	if service.overall_status == service.WARNING_STATUS:
+            alert = False
+        if service.overall_status == service.ERROR_STATUS:
+            if service.old_overall_status in (service.ERROR_STATUS, service.ERROR_STATUS):
+                alert = False 
+        if service.overall_status == service.PASSING_STATUS:
+            color = 'green'
+            if service.old_overall_status == service.WARNING_STATUS:
+                alert = False
+        else:
+            color = 'red'
+
+    	c = Context({
+            'service': service,
+            'host': settings.WWW_HTTP_HOST,
+            'scheme': settings.WWW_SCHEME,
+            'alert': alert,
+        })
+
+        message = Template(post_template).render(c)
+        self._send_post_alert(message, color=color, sender='Cabot/%s' % service.name)
+
+    def _send_hipchat_alert(self, message, color='green', sender='Cabot'):
+
+        url = env.get('POST_URL')
+
+        resp = requests.post(url, 
+        	data={
+            'from': sender[:15],
+            'message': message,
+            'notify': 1,
+            'color': color,
+            'message_format': 'text',
+        })
+
+    class HttpAlert(AlertPluginUserData):
+    name = "HttpAlert Plugin"
+    key = models.CharField(max_length=32, blank=False, verbose_name="User/Group Key")
+    alert_on_warn = models.BooleanField(default=False)
+
+
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='cabot-http-alert',
+      version='0.1',
+      description='A Cabot plugin for sending alerts over http post',
+      author='Roberto Fernandez',
+      packages=[
+      	'cabot_http_alert_plugin'
+      ])
